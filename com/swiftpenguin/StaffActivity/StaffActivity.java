c7dadc5c66e676be04a5a69b148de04eb97e1f28
Will <williameaster@hotmail.com>
2018-03-21 23:10:24 +0000

    Fixed Spacing

--- com/swiftpenguin/StaffActivity/StaffActivity.java
+++ com/swiftpenguin/StaffActivity/StaffActivity.java
@@ -65,10 +65,9 @@ public class StaffActivity extends JavaPlugin implements Listener {
         Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             public void run() {
                 long CurrentTime = System.currentTimeMillis() / 1000;
-                
+
                 for (String uuid : getConfig().getConfigurationSection("StaffUUIDS").getKeys(false)) {
                     int LastTimeStamp = getConfig().getInt("StaffUUIDS." + uuid + ".timeStamp");
-
                     long Difference = CurrentTime - LastTimeStamp;
 
                     if (Difference >= 432000) {
